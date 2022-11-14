46870354ac948c9d6c2dff6a0f7eada765e18603
Susana <susanab20020911@gmail.com>
2022-11-14 18:36:22 -0500

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 import AboutMe from './components/AboutMe';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <AboutMe/>
+      <Sidebar />
+      <Router>
+        <Routes>
+          <Route path="/User" element={<AboutMe />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/SidebarData.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const SidebarData = [
+    {
+        title: " User",
+        link: "/User",
+    },
+    {
+        title: " Home",
+        link: "/Home",
+    },
+    {
+        title: " Post",
+        link: "/Post",
+    },
+    {
+        title: " Sign In",
+        link: "/SignIn",
+    },
+]
